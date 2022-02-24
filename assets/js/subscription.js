d20de4af0ce0699b44fb8cd8b2fe16cac1048cc6
Hallysonjp <hallysonjp@gmail.com>
2022-02-24 18:50:05 -0300

    s

--- assets/js/subscription.js
+++ assets/js/subscription.js
@@ -2,6 +2,7 @@
  * This adapter creator export an adapter that shows a promotional message when the conversation window is shown.
  */
 function subscriptionMessage() {
+    console.log('aqui');
     return function (chatBot) {
         chatBot.subscriptions.onSendMessage(function (messageData, next) {
             messageData.message = messageData.message.replace(regex, replaceWith);
