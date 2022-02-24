46ced5854fa85c9834668cd1cf0793d03a27317f
Hallysonjp <hallysonjp@gmail.com>
2022-02-24 18:45:06 -0300

    sendMessage

--- assets/js/subscription.js
+++ assets/js/subscription.js
@@ -3,16 +3,10 @@
  */
 function subscriptionMessage() {
     return function (chatBot) {
-        chatBot.subscriptions.onDisplayChatbotMessage(function (messageData, next) {
-            console.log(messageData.message);
-            // BEFORE DisplayChatbotMessage action execution
-            if (messageData.message == "Sorry, we couldn't find any answers to your question.") {
-                console.log("hidden message")
-                return
-            } else {
-                messageData.message = 'Custom chatbot message! ' + messageData.message;
-                return next(messageData);
-            }
-        });
+        chatBot.subscriptions.onSendMessage(function (messageData, next) {
+            messageData.message = messageData.message.replace(regex, replaceWith);
+            return next(messageData);
+        }
+        );
     };
 }
