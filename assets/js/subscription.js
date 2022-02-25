74010ec19fff497e6fcab4c94ab32b8ce136f4c5
Hallysonjp <hallysonjp@gmail.com>
2022-02-25 07:05:30 -0300

    a

--- assets/js/subscription.js
+++ assets/js/subscription.js
@@ -3,13 +3,22 @@
  */
 function subscriptionMessage() {
     return function (chatBot) {
-        console.log(chatbot);
-        console.log('aqui');
-        chatBot.subscriptions.onSendMessage(function (messageData, next) {
-            messageData.message = messageData.message.replace(regex, replaceWith);
-            console.log(messageData);
-            return next(messageData);
-        }
-        );
+        // console.log(chatbot);
+        // console.log('aqui');
+        // chatBot.subscriptions.onSendMessage(function (messageData, next) {
+        //     messageData.message = messageData.message.replace(regex, replaceWith);
+        //     console.log(messageData);
+        //     return next(messageData);
+        // }
+        // );
+
+        chatBot.subscriptions.onShowConversationWindow(function (next) {
+            const SideWindow = {
+                sideWindowContent: "<p>Hi! check out this amazing article </p><a href='https://www.fakeLinkTo.com/amazingArticle/'>Article</a>",
+                sideWindowTitle: "Special promotion",
+            }
+            chatBot.actions.showSideWindow(SideWindow);
+            return next();
+        });
     };
 }
