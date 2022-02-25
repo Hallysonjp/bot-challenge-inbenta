a8dc537e26c2f3506a908af0843c5cff212caeb2
Hallyson <hallysonjp@gmail.com>
2022-02-25 11:23:58 -0300

    data

--- assets/js/subscription.js
+++ assets/js/subscription.js
@@ -1,25 +1,14 @@
 /**
  * This adapter creator export an adapter that shows a promotional message when the conversation window is shown.
  */
-function subscriptionMessage() {
-    console.log('aqui');
+function receiveData(chooseColor) {
     return function (chatBot) {
-        // console.log(chatbot);
-        // console.log('aqui');
-        // chatBot.subscriptions.onSendMessage(function (messageData, next) {
-        //     messageData.message = messageData.message.replace(regex, replaceWith);
-        //     console.log(messageData);
-        //     return next(messageData);
-        // }
-        // );
-
-        chatBot.subscriptions.onShowConversationWindow(function (next) {
-            const SideWindow = {
-                sideWindowContent: "<p>Hi! check out this amazing article </p><a href='https://www.fakeLinkTo.com/amazingArticle/'>Article</a>",
-                sideWindowTitle: "Special promotion",
+        chatBot.subscriptions.onSendMessage(
+            function (messagedata, next) {
+                console.log(chooseColor);
+                console.log(messagedata);
+                return next();
             }
-            chatBot.actions.showSideWindow(SideWindow);
-            return next();
-        });
-    };
+        );
+    }
 }
